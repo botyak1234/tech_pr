92692818aa0f01cee8e9624c81d171ad05905759
BOTYA <danilk1102@gmail.com>
2024-09-22 15:38:46 +0400

    help

---
+++ main.py
@@ -0,0 +1,10 @@
+n = int(input())
+while n % 2 == 0:
+        print(2, end=" ")
+        n //= 2
+for i in range(3, int(n ** (1/2)) + 1, 2):
+        while n % i == 0:
+            print(i, end=" ")
+            n //= i
+if n > 2:
+    print(n, end=" ")
